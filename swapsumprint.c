9cf33ff20e008ab66b1a291260e533d748690fb9
Rashik787708 <mohamed786rashik@gmail.com>
2026-06-17 20:29:21 +0530

    Initial commit

---
+++ swapsumprint.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int sum(int a,int b){
+    return a+b;
+}
+
+int main(){
+    printf("Hello world!\n");
+    int x,y;
+    scanf("%d %d",&x,&y);
+    x = x + y;
+    y= x - y;
+    x=x-y;
+    printf("%d %d\n",x,y);
+    printf("%d",sum(x,y));
+
+}
